bcc0656ddfbefb0e6c7a52d0ec2f24c71ee1e9ca
Sihan Wang <wsihan@vmware.com>
2017-06-22 22:43:21 +0000

    Add api service into column
    This is a stractch patch for column api by using flask api.
    Three routes:
        1. GET: runs/id (return a run)
        2. POST: runs (trigger a new run)
        3. GET: runs (return runs list)
    
    I would update the route as we update the swagger file
    
    Welcome comments!
    
    Signed-off-by: Sihan Wang <wsihan@vmware.com>

---
+++ column/api/controller/run_controller.py
@@ -0,0 +1,75 @@
+import logging
+import uuid
+
+from flask import request
+import flask_restful
+from flask_restful import abort
+from six.moves import http_client
+
+from column.api import backend
+from column.api import manager
+from column.api.common import utils
+
+LOG = logging.getLogger(__name__)
+
+run_post_schema = {
+    'playbook_path': {'type': 'string', 'required': True},
+    'inventory_file': {'type': 'string'},
+    'options': {
+        'type': 'dict',
+        'schema': {
+            'become_user': {'type': 'string'},
+            'private_key_file': {'type': 'string'},
+            'tags': {'type': 'list'},
+            'skip_tags': {'type': 'list'},
+            'susbset': {'type': 'string'},
+            'connection': {'type': 'string'},
+            'become_method': {'type': 'string'}
+        }
+    }
+}
+
+
+class Run(flask_restful.Resource):
+    """Column api run resource class
+
+    Attributes:
+        backend_store (obj): backend interface for retrieving run info
+    """
+
+    def __init__(self):
+        self.backend_store = backend.get_store()
+
+    def get(self, id):
+        """Get run by id"""
+        run = self.backend_store.get_run(id)
+        if not run:
+            return abort(404, message="Run {} doesn't exist".format(id))
+        return run
+
+
+class RunList(flask_restful.Resource):
+    """Column api runlist resource class
+
+    Attributes:
+        backend_store (obj): backend interface for retrieving run info
+        manager (obj): manager interface for calling column library method
+                       to run playbook
+    """
+
+    def __init__(self):
+        self.backend_store = backend.get_store()
+        self.manager = manager.get_manager('run')
+
+    def get(self):
+        """Get run list"""
+        LOG.info('Returning all ansible runs')
+        return self.backend_store.list_runs()
+
+    @utils.validator(run_post_schema, http_client.BAD_REQUEST)
+    def post(self):
+        """Trigger a new run"""
+        run_payload = request.get_json(silent=True)
+        run_payload['id'] = str(uuid.uuid4())
+        LOG.info('Triggering new ansible run %s', run_payload['id'])
+        return self.manager.create_run(run_payload)
