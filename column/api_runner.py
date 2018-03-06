6eb67d9461835822334820f1d157b348c6d18041
Sihan Wang <wsihan@vmware.com>
2018-03-06 01:05:08 +0000

    Add catch AnsibleParserError in column
    Currnetly column will not report ERROR in run if ansible raise
    AnsibleParserError.
    
    Signed-off-by: Sihan Wang <wsihan@vmware.com>

--- column/api_runner.py
+++ column/api_runner.py
@@ -8,6 +8,7 @@ import six
 from ansible import constants
 from ansible.executor import playbook_executor
 from ansible.executor import task_queue_manager
+from ansible import errors
 from ansible import inventory
 from ansible.parsing import dataloader
 from ansible.parsing.splitter import parse_kv
@@ -104,8 +105,10 @@ class APIRunner(runner.Runner):
         # There is no public API for adding callbacks, hence we use a private
         # property to add callbacks
         pbex._tqm._callback_plugins.extend(self._callbacks)
-
-        status = pbex.run()
+        try:
+            status = pbex.run()
+        except errors.AnsibleParserError as e:
+            raise exceptions.ParsePlaybookError(msg=str(e))
         stats = pbex._tqm._stats
         failed_results = errors_callback.failed_results
         result = self._process_stats(stats, failed_results)
